98641d4a05706c8560ea741ad59c8795ad810017
Piotr567 <61947900+Piotr567@users.noreply.github.com>
2020-07-29 20:26:30 +0200

    Add files via upload

---
+++ 13.Sign Znak liczby całkowitej.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+int sign (int value)
+{
+  if (value < 0)
+  {
+      return -1;
+  }
+  if (value > 0)
+  {
+      return 1;
+  }
+  if (value == 0)
+  {
+      return 0;
+  }
+  return 0;
+}
+
+int main()
+{
+    std::cout << sign(0) << std::endl;
+}
